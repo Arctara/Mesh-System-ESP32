3e074a8a015e786361bdfc7937409dee9e8d2591
Arctara <Merza.bolivar@Gmail.com>
2022-05-17 14:17:48 +0700

    Add init and isFirstRun

--- lib/eeprom_cmp/eeprom_cmp.cpp
+++ lib/eeprom_cmp/eeprom_cmp.cpp
@@ -1,1 +1,15 @@
 #include "eeprom_cmp.h"
+
+void EEPROM_init() { EEPROM.begin(EEPROM_SIZE); }
+
+void EEPROM_isFirstRun() {
+  char hasInit = EEPROM.read(HAS_INIT);
+  if (hasInit == 255) {
+    Serial.println("Initializing Everything");
+
+    SYSTEM_firstRunWizard();
+
+    EEPROM.write(HAS_INIT, 1);
+    EEPROM.commit();
+  }
+}
