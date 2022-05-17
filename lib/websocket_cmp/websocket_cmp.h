959c899c97180380cd154630292bc51ea1a588fc
Arctara <Merza.bolivar@Gmail.com>
2022-05-17 16:00:58 +0700

    Add WebSocket Library

--- lib/websocket_cmp/websocket_cmp.h
+++ lib/websocket_cmp/websocket_cmp.h
@@ -1,4 +1,24 @@
 #ifndef WEBSOCKET_CMP
 #define WEBSOCKET_CMP
 
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+#include "firebase_cmp.h"
+#include "global_cmp.h"
+#include "schedule_cmp.h"
+#include "spiffs_cmp.h"
+#include "time_cmp.h"
+#include "wifi_cmp.h"
+
+extern String target;
+extern bool conditionToSendWebsocket;
+
+void WS_init();
+void WS_loop();
+void WS_sendMessage();
+void WS_onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
+                AwsEventType type, void *arg, uint8_t *data, size_t len);
+void WS_handleMessage(void *arg, uint8_t *data, size_t len);
+
 #endif
