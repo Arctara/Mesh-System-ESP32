857d1311389903798c6f82dd615f3be631e14755
Arctara <Merza.bolivar@Gmail.com>
2022-05-17 14:17:48 +0700

    Add init and isFirstRun

--- lib/eeprom_cmp/eeprom_cmp.h
+++ lib/eeprom_cmp/eeprom_cmp.h
@@ -1,4 +1,16 @@
 #ifndef EEPROM_CMP
 #define EEPROM_CMP
 
+#include <EEPROM.h>
+
+#include "system_cmp.h"
+
+//$ Define EEPROM Size
+#define EEPROM_SIZE 512
+//$ Config Address Saved in EEPROM
+#define HAS_INIT 0
+
+void EEPROM_init();
+void EEPROM_isFirstRun();
+
 #endif
