ffb1ed6df38d47da4712ce2769352cff78afec55
Arctara <Merza.bolivar@Gmail.com>
2022-05-17 14:18:06 +0700

    Add init and now

--- lib/time_cmp/time_cmp.cpp
+++ lib/time_cmp/time_cmp.cpp
@@ -1,1 +1,11 @@
 #include "time_cmp.h"
+
+RTC_DS3231 rtc;
+
+void TIME_init() {
+  if (!rtc.begin()) {
+    Serial.println("GLOBAL: RTC Not Connected");
+  }
+}
+
+DateTime TIME_now() { return rtc.now(); }
--- lib/time_cmp/time_cmp.h
+++ lib/time_cmp/time_cmp.h
@@ -1,4 +1,9 @@
 #ifndef TIME_CMP
 #define TIME_CMP
 
+#include <RTClib.h>
+
+void TIME_init();
+DateTime TIME_now();
+
 #endif
