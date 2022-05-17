bf43f64904a96fa61609f7dc94e7271fc42b2692
Arctara <Merza.bolivar@Gmail.com>
2022-05-17 16:00:06 +0700

    Add Lot of Functions

--- lib/firebase_cmp/firebase_cmp.h
+++ lib/firebase_cmp/firebase_cmp.h
@@ -1,4 +1,43 @@
 #ifndef FIREBASE_CMP
 #define FIREBASE_CMP
 
+#include <Firebase_ESP_Client.h>
+
+#include "addons/RTDBHelper.h"
+#include "addons/TokenHelper.h"
+#include "global_cmp.h"
+#include "schedule_cmp.h"
+
+//* Firebase Data Configuration
+extern FirebaseData stream;
+extern FirebaseData fbdo;
+
+//* Firebase User Configuration
+extern FirebaseAuth auth;
+extern FirebaseConfig config;
+
+//* Firebase Variable
+extern unsigned long sendDataPrevMillis;
+extern int count;
+extern volatile boolean firebaseDataChanged;
+
+//* Struct Used for Firebase Data
+struct streamData {
+  String streamPath;
+  String dataPath;
+  String dataType;
+  String eventType;
+  String data;
+};
+
+//* Initialize Struct Data
+extern streamData receivedDataFirebase;
+
+void FIREBASE_init();
+void FIREBASE_streamCallBack();
+void FIREBASE_streamTimeoutCallback();
+void FIREBASE_initStream();
+void FIREBASE_initDeviceData();
+void FIREBASE_getSchedule();
+
 #endif
