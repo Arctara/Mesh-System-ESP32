e6f891a7de7f6b133218cd4d883ce45b87ed0ce2
Arctara <Merza.bolivar@Gmail.com>
2022-05-17 14:17:28 +0700

    Add init and print

--- lib/display_cmp/display_cmp.cpp
+++ lib/display_cmp/display_cmp.cpp
@@ -1,1 +1,26 @@
 #include "display_cmp.h"
+
+SSD1306 display(0x3C, 21, 22);
+QRcode qrcode(&display);
+
+void DISPLAY_init() {
+  display.init();
+  display.write("Hello!");
+  display.display();
+
+  qrcode.init();
+}
+
+void DISPLAY_printQRCode() {
+  String apName = AP_SSID;
+  String apPass = AP_PASS;
+  String stationIP = WiFi.localIP().toString();
+  String apIP = WiFi.softAPIP().toString();
+
+  display.clear();
+  String dataToSend = "{\"apName\":\"" + apName + "\", \"apPass\": \"" +
+                      apPass + "\", \"serverName\": \"" + WiFi.macAddress() +
+                      "\", \"stationIP\": \"" + stationIP + "\", \"apIP\": \"" +
+                      apIP + "\"}";
+  qrcode.create(dataToSend);
+}
