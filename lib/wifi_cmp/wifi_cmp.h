3bbf62ca0066d0e74bfa89ff2e1069f688a75a2c
Arctara <Merza.bolivar@Gmail.com>
2022-05-17 14:18:15 +0700

    Add AP Config

--- lib/wifi_cmp/wifi_cmp.h
+++ lib/wifi_cmp/wifi_cmp.h
@@ -1,4 +1,13 @@
 #ifndef WIFI_CMP
 #define WIFI_CMP
 
+#include <WiFi.h>
+
+//$ Access Point Configuration
+#define AP_SSID "ALiVe_AP"
+#define AP_PASS "LeTS_ALiVe"
+#define AP_CHANNEL 1
+#define AP_DISCOVERABLE 0
+#define AP_MAX_CONNECTION 10
+
 #endif
