ff7e9a419a934557c2d06d94566730f1362b16e5
Arctara <Merza.bolivar@Gmail.com>
2022-05-17 14:33:26 +0700

    Add init and getWiFiCred

--- lib/spiffs_cmp/spiffs_cmp.cpp
+++ lib/spiffs_cmp/spiffs_cmp.cpp
@@ -1,1 +1,38 @@
 #include "spiffs_cmp.h"
+
+#include <ArduinoJson.h>
+
+String wifi_ssid = "";
+String wifi_pass = "";
+
+void SPIFFS_init() {
+  if (!SPIFFS.begin(true)) {
+    Serial.println("SPIFFS: Error Mount SPIFFS");
+    return;
+  }
+}
+
+void SPIFFS_getWiFiCred() {
+  //* WiFi Setup Configuration
+  String wifi_buffer;
+  DynamicJsonDocument wifi_cred(256);
+
+  File file = SPIFFS.open("/wifi_cred.json");
+
+  if (!file) {
+    Serial.println("SPIFFS: Error Open File 2");
+    return;
+  }
+
+  while (file.available()) {
+    wifi_buffer += file.readString();
+  }
+
+  deserializeJson(wifi_cred, wifi_buffer);
+
+  wifi_ssid = wifi_cred["ssid"].as<String>();
+  wifi_pass = wifi_cred["pass"].as<String>();
+
+  Serial.println(wifi_ssid);
+  Serial.println(wifi_pass);
+}
--- lib/spiffs_cmp/spiffs_cmp.h
+++ lib/spiffs_cmp/spiffs_cmp.h
@@ -1,4 +1,12 @@
 #ifndef SPIFFS_CMP
 #define SPIFFS_CMP
 
+#include <SPIFFS.h>
+
+extern String wifi_ssid;
+extern String wifi_pass;
+
+void SPIFFS_init();
+void SPIFFS_getWiFiCred();
+
 #endif
