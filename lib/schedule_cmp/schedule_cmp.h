2031d6b34b9f397263c03c75f6c6e78949420f3e
Arctara <Merza.bolivar@Gmail.com>
2022-05-17 16:00:21 +0700

    Add Schedule-ing data

--- lib/schedule_cmp/schedule_cmp.h
+++ lib/schedule_cmp/schedule_cmp.h
@@ -1,4 +1,37 @@
 #ifndef SCHEDULE_CMP
 #define SCHEDULE_CMP
 
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+#include <List.hpp>
+
+#include "global_cmp.h"
+
+struct schedule {
+  String id;
+  bool active = false;
+  bool subActive = false;
+  bool moreSubActive = false;
+  String target;
+  String targetId;
+  String trigger;
+  String timeMode;
+  String startHour;
+  String startMinute;
+  String stopHour;
+  String stopMinute;
+  String lengthOn;
+  String lengthOff;
+  String delay;
+  String sensorId;
+  String activeCondition;
+  unsigned long prevMillisOn = 0;
+  unsigned long prevMillisOff = 0;
+};
+
+extern List<schedule> schedules;
+
+void SCHEDULE_build(String source);
+
 #endif
