55e61d50b289e4cee5063da00f2cc4b81613e5cc
Arctara <Merza.bolivar@Gmail.com>
2022-06-03 21:57:37 +0700

    Add initialization for offline data

--- lib/system_cmp/system_cmp.cpp
+++ lib/system_cmp/system_cmp.cpp
@@ -3,8 +3,11 @@
 void SYSTEM_init() { Serial.begin(115200); }
 
 void SYSTEM_firstRunWizard() {
-  // initWiFiFile();
-  // initMainFile();
+  SPIFFS_initWiFiFile();
+  SPIFFS_initLampData();
+  SPIFFS_initPlugData();
+  SPIFFS_initScheduleData();
+  SPIFFS_initSensorData();
 }
 
 void SYSTEM_restart() { ESP.restart(); }
