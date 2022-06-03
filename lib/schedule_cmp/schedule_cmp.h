26b93186900d8d1fd963c950fc72b6abf829fab8
Arctara <Merza.bolivar@Gmail.com>
2022-06-03 21:57:01 +0700

    Rename struct

--- lib/schedule_cmp/schedule_cmp.h
+++ lib/schedule_cmp/schedule_cmp.h
@@ -4,16 +4,18 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <Firebase_ESP_Client.h>
+#include <ESPAsyncWebServer.h>
 
 #include <List.hpp>
 
 #include "firebase_cmp.h"
 #include "global_cmp.h"
 #include "time_cmp.h"
+#include "spiffs_cmp.h"
 #include "websocket_cmp.h"
 #include "wifi_cmp.h"
 
-struct schedule {
+struct scheduleData {
   String id;
   bool active = false;
   bool subActive = false;
@@ -35,31 +37,31 @@ struct schedule {
   unsigned long prevMillisOff = 0;
 };
 
-extern List<schedule> schedules;
+extern List<scheduleData> schedules;
 
 void SCHEDULE_build(String source);
-void SCHEDULE_update(int index, schedule schedule);
-void SCHEDULE_turnDevice(schedule schedule, bool condition);
+void SCHEDULE_update(int index, scheduleData schedule);
+void SCHEDULE_turnDevice(scheduleData schedule, bool condition);
 
-bool SCHEDULE_isTimeBased(schedule schedule);
-bool SCHEDULE_isInterval(schedule schedule);
-bool SCHEDULE_isDelay(schedule schedule);
+bool SCHEDULE_isTimeBased(scheduleData schedule);
+bool SCHEDULE_isInterval(scheduleData schedule);
+bool SCHEDULE_isDelay(scheduleData schedule);
 
-bool SCHEDULE_inTime(schedule schedule);
-bool SCHEDULE_outTime(schedule schedule);
+bool SCHEDULE_inTime(scheduleData schedule);
+bool SCHEDULE_outTime(scheduleData schedule);
 
-bool SCHEDULE_isActive(schedule schedule);
+bool SCHEDULE_isActive(scheduleData schedule);
 
-bool SCHEDULE_isTargetLamp(schedule schedule);
-bool SCHEDULE_isTargetPlug(schedule schedule);
+bool SCHEDULE_isTargetLamp(scheduleData schedule);
+bool SCHEDULE_isTargetPlug(scheduleData schedule);
 
-bool SCHEDULE_isSubActive(schedule schedule);
-bool SCHEDULE_isMoreSubActive(schedule schedule);
+bool SCHEDULE_isSubActive(scheduleData schedule);
+bool SCHEDULE_isMoreSubActive(scheduleData schedule);
 
-bool SCHEDULE_isInLengthOn(schedule schedule);
-bool SCHEDULE_isInLengthOff(schedule schedule);
+bool SCHEDULE_isInLengthOn(scheduleData schedule);
+bool SCHEDULE_isInLengthOff(scheduleData schedule);
 
-bool SCHEDULE_isOutDelayTime(schedule schedule);
+bool SCHEDULE_isOutDelayTime(scheduleData schedule);
 
 void SCHEDULE_checkIfDelayable(String deviceId);
 void SCHEDULE_cancelDelay(String deviceId);
