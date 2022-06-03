9a022d25bc74c781cc6096f1f36156a7ee9c480b
Arctara <Merza.bolivar@Gmail.com>
2022-06-03 21:57:20 +0700

    Add offline data

--- lib/spiffs_cmp/spiffs_cmp.h
+++ lib/spiffs_cmp/spiffs_cmp.h
@@ -9,9 +9,24 @@ extern String wifi_pass;
 
 void SPIFFS_init();
 void SPIFFS_initWiFiFile();
+void SPIFFS_initLampData();
+void SPIFFS_initPlugData();
+void SPIFFS_initScheduleData();
+void SPIFFS_initSensorData();
 
 void SPIFFS_getWiFiCred();
-
 void SPIFFS_setWiFiCred(String ssid, String pass);
+
+String SPIFFS_getLampData();
+void SPIFFS_setLampData(String source);
+
+String SPIFFS_getPlugData();
+void SPIFFS_setPlugData(String source);
+
+String SPIFFS_getScheduleData();
+void SPIFFS_setScheduleData(String source);
+
+String SPIFFS_getSensorData();
+void SPIFFS_setSensorData(String source);
 
 #endif
