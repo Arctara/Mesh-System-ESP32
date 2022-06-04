1025c6a8bda17a7af41d269983e27a1f83d3086c
Arctara <Merza.bolivar@Gmail.com>
2022-06-04 22:17:43 +0700

    Add LED Functionality

--- lib/system_cmp/system_cmp.h
+++ lib/system_cmp/system_cmp.h
@@ -4,9 +4,12 @@
 #include <Arduino.h>
 
 #include "spiffs_cmp.h"
+#include "global_cmp.h"
 
 void SYSTEM_init();
 void SYSTEM_firstRunWizard();
 void SYSTEM_restart();
+void SYSTEM_blinkLED(int ledPin);
+void SYSTEM_turnLED(int ledPin, bool condition);
 
 #endif
