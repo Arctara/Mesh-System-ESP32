af10b76b9c0fc5ad36b6b8a904ef37054d08fb59
Arctara <Merza.bolivar@Gmail.com>
2022-06-04 22:17:32 +0700

    Add LED Functionality

--- lib/system_cmp/system_cmp.cpp
+++ lib/system_cmp/system_cmp.cpp
@@ -1,6 +1,11 @@
 #include "system_cmp.h"
 
-void SYSTEM_init() { Serial.begin(115200); }
+void SYSTEM_init() {
+  Serial.begin(115200);
+  pinMode(ORANGE_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+}
 
 void SYSTEM_firstRunWizard() {
   SPIFFS_initWiFiFile();
@@ -11,3 +16,13 @@ void SYSTEM_firstRunWizard() {
 }
 
 void SYSTEM_restart() { ESP.restart(); }
+
+void SYSTEM_blinkLED(int ledPin) {
+  digitalWrite(ledPin, HIGH);
+  delay(50);
+  digitalWrite(ledPin, LOW);
+}
+
+void SYSTEM_turnLED(int ledPin, bool condition) {
+  digitalWrite(ledPin, condition);
+}
